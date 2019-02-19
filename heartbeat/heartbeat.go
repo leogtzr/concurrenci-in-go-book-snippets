b2d1ceaef04fe531d99b7c8760751bc6ad4528bb
leogtzr <leogutierrezramirez@gmail.com>
2019-02-19 10:05:06 -0700

    baseline

--- heartbeat/heartbeat.go
+++ heartbeat/heartbeat.go
@@ -49,5 +49,25 @@ func heartbeat(
 }
 
 func main() {
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() { close(done) })
 
+	const timeout = 2*time.Second
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok == false {
+				return
+			}
+			fmt.Println("pulse")
+		case r, ok := <-results:
+			if ok == false {
+				return
+			}
+			fmt.Printf("results %v\n", r.Second())
+		case <-time.After(timeout):
+			return
+		}
+	}
 }
